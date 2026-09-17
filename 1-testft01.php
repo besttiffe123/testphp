36da53cf86fa244cba842dbcceccfe506fbd9b52
besttiffe123 <pitthawat99chankaew@gmail.com>
2026-09-17 14:02:23 +0700

    Commit

---
+++ 1-testft01.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Writting PHP Function</title>
+</head>
+<body>
+    <h2>Frist time for PhP</h2>
+
+    <?php
+    /* Defining */
+        function writeMessage() {
+            echo "You are really a nice person, Have a nice time!";
+        }
+    /*calling*/
+        writeMessage();
+    ?>
+</body>
+</html>
