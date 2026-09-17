972f80ab6dd230a67c25cf1b34ab5d0206c7e113
besttiffe123 <pitthawat99chankaew@gmail.com>
2026-09-17 14:22:12 +0700

    commit

---
+++ 3-testftbyRef.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        function addFive($num){
+            $num += 5;
+        }
+
+        function addSix(&$num){
+            $num += 6;
+        }
+
+        $orignum =10;
+        addFive($orignum);
+
+        echo "orignum Value is $orignum<br />";
+
+        
+        addSix($orignum);
+
+        echo "orignum Value is $orignum<br />";
+
+    ?>
+</body>
+</html>
